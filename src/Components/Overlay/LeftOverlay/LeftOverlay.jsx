54c96c5665d717e91d3d9f7a8402e7dfd9d2f1ca
Dariush <d.bakhtvar@gmail.com>
2022-08-28 16:36:41 +0430

    responsive style

--- src/Components/Overlay/LeftOverlay/LeftOverlay.jsx
+++ src/Components/Overlay/LeftOverlay/LeftOverlay.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import style from './LeftOverlay.module.scss';
 const LeftOverlay = ({ isActive, setActive }) => {
   return (
-    <div className={style.LeftOverlay}>
-      <button onClick={setActive}>View Task</button>
+    <div className={`${style.LeftOverlay}`}>
+      <div className={`${style.Panel} ${isActive && style.fadeIn}`}>
+        <button onClick={setActive}>View Task</button>
+      </div>
     </div>
   )
 }
