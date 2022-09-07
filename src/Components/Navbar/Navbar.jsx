4b036fa25194ab2abd59a3a2edd9fbe0988a751b
Dariush <d.bakhtvar@gmail.com>
2022-09-07 22:30:00 +0430

    add search bar style and use css transition group

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,50 +1,85 @@
 import style from './Navbar.module.scss';
 import { AiFillGithub, AiFillSetting, AiOutlineLogout } from 'react-icons/ai';
 import { MdOutlineForwardToInbox, MdNotifications } from 'react-icons/md';
-import withActive from '../Hoc/withActive';
-const Navbar = ({ isActive, setActive }) => {
-  const DropDown = () => {
-    return isActive && (
-      <ul className={style.submenu}>
-        <li className={style.userInfo}>
-          <AiFillGithub />
-          <span>Dariush Bakhtvar</span>
-        </li>
-        <li>
-          <MdNotifications />
-          <span>notification</span>
-        </li>
-        <li>
-          <MdOutlineForwardToInbox />
-          <span>inbox</span>
-        </li>
-        <li>
-          <AiFillSetting />
-          <span>
-            setting
-          </span>
-        </li>
-        <li>
-          <AiOutlineLogout />
-          <span>
-            logout
-          </span>
-        </li>
-      </ul>
-    );
-  }
+// import withActive from '../Hoc/withActive';
+import { CSSTransition } from 'react-transition-group';
+import '../../asset/Sass/modal.scss';
+import { useState } from 'react';
+const Navbar = () => {
+  const [isShow, setIsShow] = useState(false);
+  // const Navbar = ({ isActive, setActive }) => {
+  // const DropDown = () => {
+  //   return isShow && (
+  //     <ul className={style.submenu}>
+  //       <li className={style.userInfo}>
+  //         <AiFillGithub />
+  //         <span>Dariush Bakhtvar</span>
+  //       </li>
+  //       <li>
+  //         <MdNotifications />
+  //         <span>notification</span>
+  //       </li>
+  //       <li>
+  //         <MdOutlineForwardToInbox />
+  //         <span>inbox</span>
+  //       </li>
+  //       <li>
+  //         <AiFillSetting />
+  //         <span>
+  //           setting
+  //         </span>
+  //       </li>
+  //       <li>
+  //         <AiOutlineLogout />
+  //         <span>
+  //           logout
+  //         </span>
+  //       </li>
+  //     </ul>
+  //   );
+  // }
   return (
     <nav className={style.Navbar}>
       <div className={style.Logo}>
         <img src={require('../../asset/Img/Logo.png')} alt="Logo" />
       </div>
-      <div className={style.profile} onClick={setActive}>
+      <div className={style.profile} onClick={() => setIsShow(!isShow)}>
         <img src={require('../../asset/Img/Profile.jpg')} alt="Profile" />
         <span className={style.status}></span>
-        {DropDown()}
+        <CSSTransition in={isShow} classNames='modal' timeout={600} unmountOnExit >
+          {/* {DropDown()}
+           */}
+          <ul className={style.submenu}>
+            <li className={style.userInfo}>
+              <AiFillGithub />
+              <span>Dariush Bakhtvar</span>
+            </li>
+            <li>
+              <MdNotifications />
+              <span>notification</span>
+            </li>
+            <li>
+              <MdOutlineForwardToInbox />
+              <span>inbox</span>
+            </li>
+            <li>
+              <AiFillSetting />
+              <span>
+                setting
+              </span>
+            </li>
+            <li>
+              <AiOutlineLogout />
+              <span>
+                logout
+              </span>
+            </li>
+          </ul>
+        </CSSTransition>
       </div>
     </nav>
   )
 }
 
-export default withActive(Navbar);
+// export default withActive(Navbar);
+export default Navbar;
--- src/Components/Search_Filter/Search.jsx
+++ src/Components/Search_Filter/Search.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import { CSSTransition } from 'react-transition-group'
+import { useState } from 'react';
 import style from './Search.module.scss';
-import { CgSearchLoading } from 'react-icons/cg';
 import { MdManageAccounts } from 'react-icons/md';
+import { BsUiChecks } from 'react-icons/bs'
+import { IoMdTrash } from 'react-icons/io'
+import '../../asset/Sass/modal.scss';
 const Search = () => {
+  const [isShow, setIsShow] = useState(false);
   return (
     <div className={style.searchWrapper}>
       <div className={style.searchBox}>
-        <input type="text" placeholder='جستجو' />
-        <CgSearchLoading />
+        <input type="text" placeholder='جستجو...' />
+        <span></span>
       </div>
       <ul className={style.manageTask}>
-        <li>
+        <li onClick={() => setIsShow(!isShow)}>
           <span>مدیریت کارها</span>
           <MdManageAccounts />
-          <ul className={style.subMenu}>
-            <li>انجام همه</li>
-            <li>حذف همه</li>
-          </ul>
+          <CSSTransition
+            classNames="modal"
+            timeout={600}
+            in={isShow}
+            unmountOnExit
+          >
+            <ul className={style.subMenu}>
+              <li>
+                <span>انجام همه</span>
+                <BsUiChecks />
+              </li>
+              <li>
+                <span>حذف همه</span>
+                <IoMdTrash />
+              </li>
+            </ul>
+          </CSSTransition>
         </li>
       </ul>
     </div>
